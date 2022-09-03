f00847b31b8f673af8a29ac311b4f078d1944e8a
Harmeet Matharoo <hmjatt@gmail.com>
2022-09-03 15:24:35 -0600

    Connect with mongoose

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -10,7 +10,7 @@ const getGoals = asyncHandler(async (req, res) => {
 // @desc Set goal
 // @route POST /api/goals
 // @access Private
-const setGoal = asynchandler(async (req, res) => {
+const setGoal = asyncHandler(async (req, res) => {
     if (!req.body.text) {
         res.status(400);
         throw new Error("Please add a text field");
@@ -22,14 +22,14 @@ const setGoal = asynchandler(async (req, res) => {
 // @desc Update goal
 // @route PUT /api/goals/:id
 // @access Private
-const updateGoal = asynchandler(async (req, res) => {
+const updateGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Update goal ${req.params.id}` });
 });
 
 // @desc Delete goal
 // @route DELETE /api/goals/:id
 // @access Private
-const deleteGoal = asynchandler(async (req, res) => {
+const deleteGoal = asyncHandler(async (req, res) => {
     res.status(200).json({ message: `Delete goal ${req.params.id}` });
 });
 

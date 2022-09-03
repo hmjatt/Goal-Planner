34fe0fa6da0834e46379c1ce97398c7dd220d9b5
Harmeet Matharoo <hmjatt@gmail.com>
2022-09-03 11:48:09 -0600

    Basic Express Server ✅

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const port = 5000;
+
+const app = express()
+
 console.log("This is server.js file");
+app.listen(port, () => console.log(`Server started on port ${port}`));

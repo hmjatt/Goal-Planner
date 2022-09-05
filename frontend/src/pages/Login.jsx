1441e9e665f23c422c273c025db02302b415df66
Harmeet Matharoo <hmjatt@gmail.com>
2022-09-04 21:25:50 -0600

    Login Page :white_check_mark:

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,67 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
 function Login() {
-    return <div>Login</div>;
+    const [formData, setFormData] = useState({
+        email: "",
+        password: "",
+    });
+
+    const { email, password } = formData;
+
+    const onFormChange = (e) => {
+        setFormData((prevState) => ({
+            ...prevState,
+            [e.target.name]: e.target.value,
+        }));
+    };
+
+    const onFormSubmit = (e) => {
+        e.preventDefault();
+    };
+
+    return (
+        <>
+            <section className="heading">
+                <h1>
+                    <FaSignInAlt /> Login
+                </h1>
+                <p>Login and plan those goals</p>
+            </section>
+
+            <section className="form">
+                <form onSubmit={onFormSubmit}>
+                    <div className="form-group">
+                        <input
+                            type="email"
+                            className="form-control"
+                            id="email"
+                            name="email"
+                            value={email}
+                            placeholder="Enter your email"
+                            onChange={onFormChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <input
+                            type="password"
+                            className="form-control"
+                            id="password"
+                            name="password"
+                            value={password}
+                            placeholder="Enter password"
+                            onChange={onFormChange}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <buttom type="submit" className="btn btn-block">
+                            Submit
+                        </buttom>
+                    </div>
+                </form>
+            </section>
+        </>
+    );
 }
 
 export default Login;

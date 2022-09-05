bb1e64a442a76a61dbf6f1b0ca9a676cb75c8e7e
Harmeet Matharoo <hmjatt@gmail.com>
2022-09-05 16:07:02 -0600

    create Goal Async Thunk

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import goalService from "./goalService";
 
 const initialState = {
     goals: [],
@@ -9,6 +10,23 @@ const initialState = {
 };
 
 // Create new Goal
+export const createGoal = createAsyncThunk(
+    "goals/create",
+    async (goalData, thunkAPI) => {
+        try {
+            const token = thunkAPI.getState().auth.user.token;
+            return await goalService.createGoal(goalData, token);
+        } catch (error) {
+            const message =
+                (error.response &&
+                    error.response.data &&
+                    error.response.data.message) ||
+                error.message ||
+                error.toString();
+            return thunkAPI.rejectWithValue(message);
+        }
+    }
+);
 
 export const goalSlice = createSlice({
     name: "goal",

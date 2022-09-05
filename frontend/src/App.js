a72e6a936d485b2dcf4a8952ab61d66d3cb20669
Harmeet Matharoo <hmjatt@gmail.com>
2022-09-04 20:46:35 -0600

    install `react-icons`

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Dashboard from "./pages/Dashboard"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
     return (
@@ -9,10 +9,10 @@ function App() {
             <Router>
                 <div className="container">
                     <Routes>
-						<Route path="/" element={<Dashboard />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/register" element={<Register />} />
-					</Routes>
+                        <Route path="/" element={<Dashboard />} />
+                        <Route path="/login" element={<Login />} />
+                        <Route path="/register" element={<Register />} />
+                    </Routes>
                 </div>
             </Router>
         </>

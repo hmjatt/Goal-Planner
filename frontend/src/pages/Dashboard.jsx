0ffde1bb4f1904c81987901df03f15e1bb576232
Harmeet Matharoo <hmjatt@gmail.com>
2022-09-05 20:34:01 -0600

    Fetch Goals From Server

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,27 +1,48 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 import GoalForm from "../components/GoalForm";
+import Spinner from "../components/Spinner";
+import { reset } from "../features/auth/authSlice";
+import { getGoals} from "../features/goals/goalSlice"
 
 function Dashboard() {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
 
     const { user } = useSelector((state) => state.auth);
+    const { goals, isLoading, isError, message } = useSelector(
+        (state) => state.goals
+    );
 
     useEffect(() => {
-        if (!user) {
-            navigate("login");
+        if (isError) {
+            console.log(message);
         }
-    }, [user, navigate]);
+
+        if (!user) {
+            navigate("/login");
+        }
+
+        dispatch(getGoals());
+
+        return () => {
+            dispatch(reset());
+        };
+    }, [user, navigate, isError, message, dispatch]);
+
+    if (isLoading) {
+        return <Spinner />;
+    }
 
     return (
         <>
-            <section className="headinh">
+            <section className="heading">
                 <h1>Welcome {user && user.name}</h1>
                 <p>Goals Dashboard</p>
             </section>
 
-			<GoalForm />
+            <GoalForm />
         </>
     );
 }

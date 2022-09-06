dbe26095f3d6f1b702a0aea61d64ea9472df3217
Harmeet Matharoo <hmjatt@gmail.com>
2022-09-05 20:52:47 -0600

    Delete Goals

--- frontend/src/components/GoalItem.jsx
+++ frontend/src/components/GoalItem.jsx
@@ -1,8 +1,14 @@
+import {useDispatch} from "react-redux"
+import {deleteGoal} from "../features/goals/goalSlice"
+
 function GoalItem( {goal} ) {
+	const dispatch = useDispatch();
+
     return (
         <div className="goal">
             <div>{new Date(goal.createdAt).toLocaleString("en-CA")}</div>
             <h2>{goal.text}</h2>
+			<button onClick={() => dispatch(deleteGoal(goal._id))} className="close">X</button>
         </div>
     );
 }
